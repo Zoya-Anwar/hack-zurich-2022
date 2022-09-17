f40fd2d259838b697d89cee5aa63084e4bfe94a5
Sam Hirst <45145746+Naeviant@users.noreply.github.com>
2022-09-17 13:47:33 +0200

    Apply formatting fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,45 +8,11 @@ import { Tab, Tabs } from '@mui/material';
 function App() {
 	return (
         <Router>
-            	<div style={{display:'flex',
-			height: '100vh',
-			flexDirection:'column',
-			backgroundColor: 'white',
-			width: '390px',
-
-}}> 
-
-	<div id="wrapper" style={{
-				flex:'1',
-				overflowY: 'scroll',
-				backgroundColor: 'white',
-			}}>
-                            <Routes>
+            <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/home" element={<Home />} />
                 <Route path="/cv" element={<CVUpload />} />
             </Routes>
-            </div>
-
-        
-            <div >
-            <Tabs value={'a'} onChange={(c)=>{
-
-            }} aria-label="nav tabs example">
-			
-			<Link to='/cv'>
-				
-			<Tab  label="Item One" aria-label="phone" />
-			</Link>
-
-			<Link to='/'>
-            <Tab  label="Item One"  aria-label="aa" />
-			</Link>
-            <Tab label="Item One" aria-label="cc" />
-            </Tabs>
-            </div>
-
-        </div>
         </Router>
 	);
 }
