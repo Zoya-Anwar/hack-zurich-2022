dfeab63f35ab76d28746ab6b9ac8f090d9d0c692
Ishwor Giri <ishworgiri1999@gmail.com>
2022-09-17 14:14:09 +0545

    fix frontend directory issue

--- frontend/src/pages/CVUpload/index.tsx
+++ frontend/src/pages/CVUpload/index.tsx
@@ -1,10 +1,69 @@
 import React from 'react';
+import { Card } from '../../Components/Card';
+import { SectionHeader } from '../../Components/SectionHeader';
 import './style.css';
+import { FixedSizeList } from 'react-window';
+import { EventCard } from '../../Components/EventCard';
+import { ProfileCard } from '../../Components/ProfileCard';
+import { ProfileCardSmall } from '../../Components/ProfileCardSmall';
+
+
+
+const Column = ({data, index, style }:any) => (
+	<div style={style}>
+		<Card/>
+		{data}
+	</div>
+  );
+
+  const Column2 = ({data, index, style }:any) => (
+	<div style={style}>
+		<EventCard/>
+		{data}
+	</div>
+  );
 
 function CVUpload() {
 	return (
-        <>CV</>
-	);
+<div className='flex flex-col space-y-5 p-4'>
+
+<ProfileCardSmall/>
+
+{/* <ProfileCard></ProfileCard> */}
+
+<div>
+
+<SectionHeader/>
+</div>
+
+ <FixedSizeList
+    height={300}
+    // itemCount={5}
+	itemData={[1,2,3]}
+    itemSize={300}
+    layout="horizontal"
+	itemCount={3}
+
+    width={300}
+  >
+    {Column}
+  </FixedSizeList>
+
+
+  <FixedSizeList
+    height={300}
+    // itemCount={5}
+	itemData={[1,2,3]}
+    itemSize={300}
+    layout="horizontal"
+	itemCount={3}
+
+    width={300}
+  >
+    {Column2}
+  </FixedSizeList>
+
+</div>);
 }
 
 export default CVUpload;
